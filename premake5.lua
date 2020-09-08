607fa3e412c34ffa8f98f2151a7da88abbcdbe3f
Lucas Merenciano <lucasmermar@gmail.com>
2020-09-08 20:58:04 +0200

    added entrypoint and updated premake5 script

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,24 @@
 workspace "Motoret"
 	configurations {"Debug", "Release"}
+    location "build"
 
 project "motoret"
 	kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/"
+    objdir "buid/"
 	files {
 		"src/*.h",
 		"src/*.cpp",
+        "src/*/*.h",
+        "src/*/*.cpp",
 	}
+
+    filter "configurations:Debug"
+        defines "MEREN_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "MEREN_RELEASE"
+        optimize "On"
 
