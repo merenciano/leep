2d502f0a3a84bc7eebbfe3bd926ecdefedcd8816
Lucas Merenciano <lucasmermar@gmail.com>
2021-05-04 18:26:25 +0200

    Small fix on string in order to compile with linux

--- src/core/string.h
+++ src/core/string.h
@@ -6,7 +6,11 @@
 
 namespace leep
 {
+#ifdef LEEP_LINUX
+    typedef std::string String;
+#else
     typedef std::basic_string<char, std::char_traits<char>, BuddySTL<char>> String;
+#endif
     String ToString(int32_t number);
 }
 
